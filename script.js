21b3d424f624fd1d44cdfb7c40cdb3f68a3b3ffa
SonaDrocarova <93249213+SonaDrocarova@users.noreply.github.com>
2024-07-30 12:14:07 +0200

    changed details section

--- script.js
+++ script.js
@@ -61,3 +61,11 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+function toggleQRCode(id) {
+    var qrCodes = document.getElementById(id);
+    if (qrCodes.style.display === "none") {
+        qrCodes.style.display = "flex";
+    } else {
+        qrCodes.style.display = "none";
+    }
+}
